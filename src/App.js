e581977859b96f3d392b54921cb955397839c12a
Saurabh <asaurabh2003@gmail.com>
2025-01-29 18:08:48 -0500

    Add all files

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  // Your firebase config
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Chat</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollection(query);
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const deleteMessage = async (id) => {
+    await messagesRef.doc(id).delete();
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.docs.map((doc) => (
+            <ChatMessage
+              key={doc.id}
+              id={doc.id}
+              message={doc.data()}
+              deleteMessage={deleteMessage}
+            />
+          ))}
+
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { id } = props;
+  const { text, uid, photoURL } = props.message;
+  const { deleteMessage } = props;
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || "/logo192.png"} />
+      <p>{text}</p>
+      {uid === auth.currentUser.uid && (
+        <button className="delete-button" onClick={() => deleteMessage(id)}>
+          Delete
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default App;
